843a717f2182357b9d905b0903e4da83a70c141e
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-18 01:38:03 +0300

    Prepare clip path for each frame (half done, still need polishing)

--- src/constants/menuOptions.js
+++ src/constants/menuOptions.js
@@ -2,13 +2,53 @@ import SampleMenuComponent from "../components/MenuComponents/SampleMenuComponen
 import { menuStrings } from "./menuStrings";
 
 export const menuOptions = [
-    { buttonNumber: 1, buttonText: menuStrings.menu.map, component: SampleMenuComponent },
-    { buttonNumber: 2, buttonText: menuStrings.menu.brief, component: SampleMenuComponent },
-    { buttonNumber: 3, buttonText: menuStrings.menu.load, component: SampleMenuComponent },
-    { buttonNumber: 4, buttonText: menuStrings.menu.stats, component: SampleMenuComponent },
-    { buttonNumber: 5, buttonText: menuStrings.menu.controls, component: SampleMenuComponent },
-    { buttonNumber: 6, buttonText: menuStrings.menu.audio, component: SampleMenuComponent },
-    { buttonNumber: 7, buttonText: menuStrings.menu.display, component: SampleMenuComponent },
-    { buttonNumber: 8, buttonText: menuStrings.menu.language, component: SampleMenuComponent },
-    // Add more options as needed
-  ];
+  {
+    buttonNumber: 1,
+    buttonText: menuStrings.menu.map,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 5%, 92% 11%, 95% 75%, 6.5% 83%)`
+  },
+  {
+    buttonNumber: 2,
+    buttonText: menuStrings.menu.brief,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 72%, 10% 75%)`
+  },
+  {
+    buttonNumber: 3,
+    buttonText: menuStrings.menu.load,
+    component: SampleMenuComponent,
+    frameClip: `polygon(5.6% 17.4%, 90% 5.5%, 92% 75%, 4.5% 80%)`
+  },
+  {
+    buttonNumber: 4,
+    buttonText: menuStrings.menu.stats,
+    component: SampleMenuComponent,
+    frameClip: `polygon(6% 20.2%, 86% 3%, 88% 75%, 6.5% 80%)`
+  },
+  {
+    buttonNumber: 5,
+    buttonText: menuStrings.menu.controls,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+  },
+  {
+    buttonNumber: 6,
+    buttonText: menuStrings.menu.audio,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+  },
+  {
+    buttonNumber: 7,
+    buttonText: menuStrings.menu.display,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+  },
+  {
+    buttonNumber: 8,
+    buttonText: menuStrings.menu.language,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+  },
+  // Add more options as needed
+];
