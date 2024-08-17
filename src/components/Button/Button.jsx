82dfed2482c0d3253752bfc2ccc4be151d86f255
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-17 11:53:13 +0300

    Add random clip path, change padding, change hover to be dependent on state

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,64 @@
-import styles from './Button.module.css';
+import styles from "./Button.module.css";
+import { useEffect, useState } from "react";
 
-const Button = ({buttonText='Sample', hoverFunction, selectFunction}) => {
-    return (
-        <div className={styles.buttonContainer} onMouseEnter={hoverFunction} onClick={selectFunction}>
-            {buttonText}
-        </div>
-    )
-}
+const getRandomNumber = (min, max) => {
+    return Math.random() * (max - min) + min
+  }  
+
+  const generateRandomClipPath = () => {
+    // Ensure the points are within the 20% range
+    const x1 = getRandomNumber(0, 13);
+    const y1 = getRandomNumber(0, 13);
+    const x2 = getRandomNumber(87, 100);
+    const y2 = getRandomNumber(0, 13);
+    const x3 = getRandomNumber(87, 100);
+    const y3 = getRandomNumber(87, 100);
+    const x4 = getRandomNumber(0, 13);
+    const y4 = getRandomNumber(87, 100);
+  
+    return `polygon(${x1}% ${y1}%, ${x2}% ${y2}%, ${x3}% ${y3}%, ${x4}% ${y4}%)`;
+  };
+
+  
+  const Button = ({
+  buttonText = "Sample",
+  hoverFunction,
+  selectFunction,
+  buttonNumber = 0,
+  hoveredOption = 0,
+  setHoveredOption,
+}) => {
+  const [clipPathStyle, setClipPathStyle] = useState({
+    transition: 'linear 0.1s',
+    clipPath: generateRandomClipPath(),
+});
+
+const isHovered = () => {
+    return hoveredOption === buttonNumber;
+};
+useEffect(()=> {
+  console.log(clipPathStyle);
+},[clipPathStyle]);
+
+const handleHover = () => {
+    if ( isHovered() ) return;
+    hoverFunction();
+    setHoveredOption?.(buttonNumber);
+    setClipPathStyle({ clipPath: generateRandomClipPath() });
+  };
+
+  return (
+    <div
+      className={`${styles.buttonContainer} ${
+        isHovered() ? styles.buttonContainerHover : ""
+      }`}
+      onMouseEnter={handleHover}
+      onClick={selectFunction}
+      style={clipPathStyle}
+    >
+      {buttonText}
+    </div>
+  );
+};
 
 export default Button;
