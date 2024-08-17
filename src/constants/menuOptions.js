61b7377a51e19fc6b7a52aff22c3e2be34e5503e
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-17 15:51:35 +0300

    Prepare support for languages later on

--- src/constants/menuOptions.js
+++ src/constants/menuOptions.js
@@ -1,13 +1,14 @@
 import SampleMenuComponent from "../components/MenuComponents/SampleMenuComponent";
+import { menuStrings } from "./menuStrings";
 
 export const menuOptions = [
-    { buttonNumber: 1, buttonText: "map", component: SampleMenuComponent },
-    { buttonNumber: 2, buttonText: "brief", component: SampleMenuComponent },
-    { buttonNumber: 3, buttonText: "load", component: SampleMenuComponent },
-    { buttonNumber: 4, buttonText: "stats", component: SampleMenuComponent },
-    { buttonNumber: 5, buttonText: "controls", component: SampleMenuComponent },
-    { buttonNumber: 6, buttonText: "audio", component: SampleMenuComponent },
-    { buttonNumber: 7, buttonText: "display", component: SampleMenuComponent },
-    { buttonNumber: 8, buttonText: "language", component: SampleMenuComponent },
+    { buttonNumber: 1, buttonText: menuStrings.menu.map, component: SampleMenuComponent },
+    { buttonNumber: 2, buttonText: menuStrings.menu.brief, component: SampleMenuComponent },
+    { buttonNumber: 3, buttonText: menuStrings.menu.load, component: SampleMenuComponent },
+    { buttonNumber: 4, buttonText: menuStrings.menu.stats, component: SampleMenuComponent },
+    { buttonNumber: 5, buttonText: menuStrings.menu.controls, component: SampleMenuComponent },
+    { buttonNumber: 6, buttonText: menuStrings.menu.audio, component: SampleMenuComponent },
+    { buttonNumber: 7, buttonText: menuStrings.menu.display, component: SampleMenuComponent },
+    { buttonNumber: 8, buttonText: menuStrings.menu.language, component: SampleMenuComponent },
     // Add more options as needed
   ];
---
+++ src/constants/menuStrings.js
@@ -0,0 +1,12 @@
+export const menuStrings = {
+    menu: {
+        map: "Map",
+        brief: "Brief",
+        load: "Load",
+        stats: "Stats",
+        controls: "Controls",
+        audio: "Audio",
+        display: "Display",
+        language: "Language",
+    }
+  };
