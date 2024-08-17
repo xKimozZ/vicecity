f7a6124f5d24fe448a1a59a618d16ff42de986d4
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-17 15:51:16 +0300

    Prepare modular constant option file

---
+++ src/components/MenuComponents/SampleMenuComponent.jsx
@@ -0,0 +1,9 @@
+const SampleMenuComponent = () => {
+    return (
+        <div style={{backgroundColor:"blue", width:'60vh', height:'40vh'}}>
+            hello
+        </div>
+    )
+}
+
+export default SampleMenuComponent;
---
+++ src/constants/menuOptions.js
@@ -0,0 +1,13 @@
+import SampleMenuComponent from "../components/MenuComponents/SampleMenuComponent";
+
+export const menuOptions = [
+    { buttonNumber: 1, buttonText: "map", component: SampleMenuComponent },
+    { buttonNumber: 2, buttonText: "brief", component: SampleMenuComponent },
+    { buttonNumber: 3, buttonText: "load", component: SampleMenuComponent },
+    { buttonNumber: 4, buttonText: "stats", component: SampleMenuComponent },
+    { buttonNumber: 5, buttonText: "controls", component: SampleMenuComponent },
+    { buttonNumber: 6, buttonText: "audio", component: SampleMenuComponent },
+    { buttonNumber: 7, buttonText: "display", component: SampleMenuComponent },
+    { buttonNumber: 8, buttonText: "language", component: SampleMenuComponent },
+    // Add more options as needed
+  ];
