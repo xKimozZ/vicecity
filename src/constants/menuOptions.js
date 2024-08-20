63a9dccea6c517b90105e3d4058b5522771bfb3a
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-21 02:01:00 +0300

    Update frame clip path for all

--- src/constants/menuOptions.js
+++ src/constants/menuOptions.js
@@ -6,49 +6,55 @@ export const menuOptions = [
     buttonNumber: 1,
     buttonText: menuStrings.menu.map,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 5%, 92% 11%, 95% 75%, 6.5% 83%)`
+    frameClip: `polygon(13.75% 4.88%, 89.06% 10.16%, 93.75% 79.88%, 9.53% 83%)`
   },
   {
     buttonNumber: 2,
     buttonText: menuStrings.menu.brief,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 72%, 10% 75%)`
+    frameClip: `polygon(11.86% 14.38%, 89.06% 14.51%, 95.47% 74.61%, 15.63% 75.98%)`
   },
   {
     buttonNumber: 3,
     buttonText: menuStrings.menu.load,
     component: SampleMenuComponent,
-    frameClip: `polygon(5.6% 17.4%, 90% 5.5%, 92% 75%, 4.5% 80%)`
+    frameClip: `polygon(10.41% 15.43%, 88.25% 4.72%, 93.75% 76.37%, 8.49% 78.83%)`
   },
   {
     buttonNumber: 4,
     buttonText: menuStrings.menu.stats,
     component: SampleMenuComponent,
-    frameClip: `polygon(6% 20.2%, 86% 3%, 88% 75%, 6.5% 80%)`
+    frameClip: `polygon(10.94% 16.52%, 85.94% 3.75%, 90.94% 76.17%, 11.66% 78.71%)`
   },
   {
     buttonNumber: 5,
     buttonText: menuStrings.menu.controls,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+    frameClip: `polygon(11.48% 9.38%, 92.1% 14.26%, 96.43% 74.61%, 6.88% 83.48%)`
   },
   {
     buttonNumber: 6,
     buttonText: menuStrings.menu.audio,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+    frameClip: `polygon(16.32% 10.55%, 88.1% 5.63%, 95.44% 74.41%, 8.38% 82.84%)`
   },
   {
     buttonNumber: 7,
     buttonText: menuStrings.menu.display,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+    frameClip: `polygon(15.31% 14.36%, 83.75% 4.2%, 94.38% 83.67%, 15.75% 77.38%)`
   },
   {
     buttonNumber: 8,
     buttonText: menuStrings.menu.language,
     component: SampleMenuComponent,
-    frameClip: `polygon(8% 16.5%, 90% 16.5%, 95% 75%, 10% 75%)`
+    frameClip: `polygon(15.63% 11.26%, 86.25% 4.42%, 95.16% 75.85%, 15.78% 82.57%)`
   },
-  // Add more options as needed
+  /*
+  {
+    buttonNumber: 9,
+    buttonText: menuStrings.menu.credits,
+    component: SampleMenuComponent,
+    frameClip: `polygon(8% 5%, 92% 11%, 95% 75%, 6.5% 83%)`
+  },*/
 ];
