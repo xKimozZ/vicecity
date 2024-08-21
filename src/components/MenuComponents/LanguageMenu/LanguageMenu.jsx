d7c1ffab513b1139b1548e9fea7c91e02a874a3a
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2024-08-22 01:41:24 +0300

    Prepare language menu component

---
+++ src/components/MenuComponents/LanguageMenu/LanguageMenu.jsx
@@ -0,0 +1,16 @@
+import Button from "../../Button/Button"
+import styles from "./LanguageMenu.module.css"
+
+const LanguageMenu = () => {
+    return(
+        <div className={styles.languageContainer}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+        </div>
+    )
+}
+
+export default LanguageMenu;
