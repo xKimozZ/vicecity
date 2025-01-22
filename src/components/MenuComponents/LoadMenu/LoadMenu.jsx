7f722af3204e9afc514a25dd9bf0aedba0b14f5d
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-01-22 13:07:51 +0200

    Add new savegames to the menu and remove line that caused unnecessary rerenders

--- src/components/MenuComponents/LoadMenu/LoadMenu.jsx
+++ src/components/MenuComponents/LoadMenu/LoadMenu.jsx
@@ -3,7 +3,6 @@ import { buttonGroups } from "../../../constants/buttonGroups";
 import { stringLoadSelector } from "../../../store/localizationSlice";
 import Button from "../../Button/Button";
 import styles from "./LoadMenu.module.css";
-import { languageMap } from "../../../constants/menuStrings";
 import SaveGame from "./SaveGame";
 import Cursor from "../../Cursor/Cursor";
 import { actionNames } from "../../../constants/actionNames";
@@ -39,10 +38,10 @@ const LoadMenu = () => {
           <SaveGame buttonNumber={3} slotNumber={1} saveFile={{name: "In the beginning...", date: new Date("2002-10-26 18:46:13")}}/>
           <SaveGame buttonNumber={4} slotNumber={2}/>
           <SaveGame buttonNumber={5} slotNumber={3}/>
-          <SaveGame buttonNumber={6} slotNumber={4}/>
+          <SaveGame buttonNumber={6} slotNumber={4} saveFile={{name: "All Hands on Deck!", date: new Date("2003-7-6 16:32:07")}}/>
           <SaveGame buttonNumber={7} slotNumber={5}/>
-          <SaveGame buttonNumber={8} slotNumber={6}/>
-          <SaveGame buttonNumber={9} slotNumber={7} saveFile={{name: "Keep your Friends Close...", date: new Date()}}/>
+          <SaveGame buttonNumber={8} slotNumber={6} saveFile={{name: "Cap the Collector", date: new Date("2019-12-31 23:48:59")}}/>
+          <SaveGame buttonNumber={9} slotNumber={7} saveFile={{name: "Keep your Friends Close...", date: new Date("2000-1-1 09:17:48")}}/>
           <SaveGame buttonNumber={10} slotNumber={8}/>
         </div>
       </div>
