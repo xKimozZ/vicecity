87d675dc380e26161c5283a1f234e779ea0ba2dc
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-01-25 18:20:44 +0200

    Follow up to previous commit

--- src/assets/imageImports.js
+++ src/assets/imageImports.js
@@ -1,4 +1,5 @@
 import vclogo1024 from './images/vclogo1024.png'
+import scsize from './images/scsize.png'
 
 export const imageImports = {
   global: {
@@ -11,6 +12,6 @@ export const imageImports = {
 
   },
   miscImages: {
-
+    scsize,
   },
 };
