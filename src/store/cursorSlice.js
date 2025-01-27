82a163eb46f4fa17953128b0cd24caf89a7e5b47
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-01-27 12:51:49 +0200

    Allow cursor drawer to store the last cursor factors, but currently disabled until further testing is done

--- src/store/cursorSlice.js
+++ src/store/cursorSlice.js
@@ -2,6 +2,12 @@ import { createSlice, createSelector } from "@reduxjs/toolkit";
 import generateRandomClipPath from "../utils/math/generateRandomClipPath";
 import getRandomNumber from "../utils/math/getRandomNumber";
 
+const DEFAULT_CLIP_FACTOR = 13;
+const DEFAULT_TOP_FACTOR = 0.98;
+const DEFAULT_LEFT_FACTOR = 0.99;
+const DEFAULT_WIDTH_FACTOR = 1.1;
+const DEFAULT_HEIGHT_FACTOR = 1.8;
+
 const initialState = {
   positionStyle: {
     top: "0%",
@@ -12,13 +18,15 @@ const initialState = {
   clipPathStyle: {
     clipPath: generateRandomClipPath(),
   },
+  storedFactors: {
+    storedClipFactor : DEFAULT_CLIP_FACTOR,
+    storedTopFactor : DEFAULT_TOP_FACTOR,
+    storedLeftFactor : DEFAULT_LEFT_FACTOR,
+    storedWidthFactor : DEFAULT_WIDTH_FACTOR,
+    storedHeightFactor : DEFAULT_HEIGHT_FACTOR,
+  },
+  resetToggle: true,
 };
-
-const defaultClipFactor = 13;
-const defaultTopFactor = 0.98;
-const defaultLeftFactor = 0.99;
-const defaultWidthFactor = 1.1;
-const defaultHeightFactor = 1.8;
 
 export const cursorSlice = createSlice({
   name: "cursor",
@@ -30,11 +38,11 @@ export const cursorSlice = createSlice({
         left = "0%",
         width = "0%",
         height = "0%",
-        clipFactor = defaultClipFactor,
-        topFactor = defaultTopFactor,
-        leftFactor = defaultLeftFactor,
-        widthFactor = defaultWidthFactor,
-        heightFactor = defaultHeightFactor,
+        clipFactor = state.resetToggle ? DEFAULT_CLIP_FACTOR :state.storedFactors.storedClipFactor,
+        topFactor = state.resetToggle ? DEFAULT_TOP_FACTOR :state.storedFactors.storedTopFactor,
+        leftFactor = state.resetToggle ? DEFAULT_LEFT_FACTOR :state.storedFactors.storedLeftFactor,
+        widthFactor = state.resetToggle ? DEFAULT_WIDTH_FACTOR :state.storedFactors.storedWidthFactor,
+        heightFactor = state.resetToggle ? DEFAULT_HEIGHT_FACTOR :state.storedFactors.storedHeightFactor,
       } = payload;
 
       const newPositionStyle = {
@@ -50,6 +58,15 @@ export const cursorSlice = createSlice({
 
       state.positionStyle = newPositionStyle;
       state.clipPathStyle = newClipPathStyle;
+      state.storedFactors = {
+        storedClipFactor : clipFactor,
+        storedTopFactor : topFactor,
+        storedLeftFactor : leftFactor,
+        storedWidthFactor : widthFactor,
+        storedHeightFactor : heightFactor,
+      };
+
+      // state.resetToggle = !state.resetToggle;
     },
   },
 });
