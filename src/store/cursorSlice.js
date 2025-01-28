35fc398ad7283614a36baa8c4bc4723e553da011
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-01-28 23:51:46 +0200

    Refactor cursorSlice to introduce new min/max factor variables for position and size calculations; enhance randomness in positioning logic

--- src/store/cursorSlice.js
+++ src/store/cursorSlice.js
@@ -8,6 +8,12 @@ const DEFAULT_LEFT_FACTOR = 0.99;
 const DEFAULT_WIDTH_FACTOR = 1.1;
 const DEFAULT_HEIGHT_FACTOR = 1.8;
 
+//const DEFAULT_MIN_CLIP_FACTORS = {x1: 0, y1: 0, x2: 100, y2: 0, x3: 100, y3: 100, x4: 0, y4: 100};
+const DEFAULT_MAX_TOP_FACTOR = 1.0;
+const DEFAULT_MAX_LEFT_FACTOR = 1.0;
+const DEFAULT_MIN_WIDTH_FACTOR = 1.0;
+const DEFAULT_MIN_HEIGHT_FACTOR = 1.0;
+
 const initialState = {
   positionStyle: {
     top: "0%",
@@ -24,6 +30,12 @@ const initialState = {
     storedLeftFactor : DEFAULT_LEFT_FACTOR,
     storedWidthFactor : DEFAULT_WIDTH_FACTOR,
     storedHeightFactor : DEFAULT_HEIGHT_FACTOR,
+
+    //storedMinClipFactors : DEFAULT_MIN_CLIP_FACTORS,
+    storedmaxTopFactor : DEFAULT_MAX_TOP_FACTOR,
+    storedmaxLeftFactor : DEFAULT_MAX_LEFT_FACTOR,
+    storedMinWidthFactor : DEFAULT_MIN_WIDTH_FACTOR,
+    storedMinHeightFactor : DEFAULT_MIN_HEIGHT_FACTOR,
   },
   lastTriggeredBy: {buttonNumber: 1, buttonGroup: 0},
 };
@@ -52,13 +64,19 @@ export const cursorSlice = createSlice({
         leftFactor = willUseDefaults ? DEFAULT_LEFT_FACTOR : state.storedFactors.storedLeftFactor,
         widthFactor = willUseDefaults ? DEFAULT_WIDTH_FACTOR : state.storedFactors.storedWidthFactor,
         heightFactor = willUseDefaults ? DEFAULT_HEIGHT_FACTOR : state.storedFactors.storedHeightFactor,
+
+        //minClipFactors = willUseDefaults ? DEFAULT_MIN_CLIP_FACTORS : state.storedFactors.storedMinClipFactors,
+        maxTopFactor = willUseDefaults ? DEFAULT_MAX_TOP_FACTOR : state.storedFactors.storedmaxTopFactor,
+        maxLeftFactor = willUseDefaults ? DEFAULT_MAX_LEFT_FACTOR : state.storedFactors.storedmaxLeftFactor,
+        minWidthFactor = willUseDefaults ? DEFAULT_MIN_WIDTH_FACTOR : state.storedFactors.storedMinWidthFactor,
+        minHeightFactor = willUseDefaults ? DEFAULT_MIN_HEIGHT_FACTOR : state.storedFactors.storedMinHeightFactor,
       } = payload;
 
       const newPositionStyle = {
-        top: `${top * getRandomNumber(topFactor, 1)}%`,
-        left: `${left * getRandomNumber(leftFactor, 1)}%`,
-        width: `${width * getRandomNumber(1, widthFactor)}%`,
-        height: `${height * getRandomNumber(1, heightFactor)}%`,
+        top: `${top * getRandomNumber(topFactor, maxTopFactor)}%`,
+        left: `${left * getRandomNumber(leftFactor, maxLeftFactor)}%`,
+        width: `${width * getRandomNumber(minWidthFactor, widthFactor)}%`,
+        height: `${height * getRandomNumber(minHeightFactor, heightFactor)}%`,
       };
 
       const newClipPathStyle = {
@@ -67,12 +85,20 @@ export const cursorSlice = createSlice({
 
       state.positionStyle = newPositionStyle;
       state.clipPathStyle = newClipPathStyle;
+      
       state.storedFactors = {
+
         storedClipFactor : clipFactor,
         storedTopFactor : topFactor,
         storedLeftFactor : leftFactor,
         storedWidthFactor : widthFactor,
         storedHeightFactor : heightFactor,
+
+        //storedMinClipFactors : minClipFactors,
+        storedmaxTopFactor : maxTopFactor,
+        storedmaxLeftFactor : maxLeftFactor,
+        storedMinWidthFactor : minWidthFactor,
+        storedMinHeightFactor : minHeightFactor,
       };
 
       state.lastTriggeredBy = { buttonNumber: buttonNumber, buttonGroup: buttonGroup };
