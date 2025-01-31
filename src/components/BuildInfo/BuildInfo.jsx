a54e377b78f0924481ce8d1a610c3f0d9482f7b6
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-02-01 00:25:51 +0200

    Optimize responsivity for the build info header and adjust text and sizing

--- src/components/BuildInfo/BuildInfo.jsx
+++ src/components/BuildInfo/BuildInfo.jsx
@@ -9,35 +9,42 @@ const BuildInfo = () => {
   };
 
   return (
-    <header className={styles.header} style={{opacity: isVisible ? 1 : 0}}>
+    <header
+      className={`${styles.header} arborcrestXS`}
+      style={{ opacity: isVisible ? 1 : 0 }}
+    >
       <div className={styles.headerBg} onClick={toggleVisibility}>
-        Development build. Recommended to navigate with arrow keys / ESC / Enter. Hover for more info. Copyright xKimozZ, 2024 - {new Date().getFullYear()}.
+        Development build. Recommended to navigate with arrow keys / ESC /
+        Enter. Hover for more info. Copyright xKimozZ, 2024 -{" "}
+        {new Date().getFullYear()}.
       </div>
       {isVisible && (
         <div id="disclaimer" className={styles.disclaimer}>
           <div className={styles.disclaimerContent}>
-          <div style={{textAlign: "center" }}>
-          <span style={{ color: "var(--pink)" }}>
-              GTA Vice City PS2 Frontend:{" "}
-            </span>
-            <span style={{ color: "#61dafb" }}>React Recreation. </span>
-            <span style={{ color: "var(--green)" }}>
-              <a
-                href="https://github.com/xKimozZ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Built by xKimozZ{" "}
-              </a>
-              <a
-                href="https://www.linkedin.com/in/karim-ayman-h/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                (Karim Ayman).{" "}
-              </a>
-            </span>
-          </div>
+            <div style={{ textAlign: "center" }}>
+              <span style={{ color: "var(--pink)", fontSize: "1.45em" }}>
+                GTA Vice City PS2 Frontend:{" "}
+              </span>
+              <span style={{ color: "#61dafb" }}>React Recreation. </span>
+              <span style={{ color: "var(--green)" }}>
+                <u>
+                  <a
+                    href="https://github.com/xKimozZ"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Built by xKimozZ{" "}
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/karim-ayman-h/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    (Karim Ayman).{" "}
+                  </a>
+                </u>
+              </span>
+            </div>
             <br />
             The original assets and design belong to{" "}
             <a
@@ -47,13 +54,13 @@ const BuildInfo = () => {
             >
               Rockstar Games
             </a>{" "}
-            and Take-Two Interactive, and this is a non-profit hobby project that
-            does not intend to infringe on any trademarks.
+            and Take-Two Interactive, and this is a non-profit hobby project
+            that does not intend to infringe on any trademarks.
             <br />
-            Please note the app is still under development primarily for desktop, so
-            some features might not be optimized or implemented yet. More info will be
-            available soon. If you wish to hide the header, you can click to toggle its visibility.{" "}
-            <br />
+            Please note the app is still under development primarily for
+            desktop, so some features might not be optimized or implemented yet.
+            More info will be available soon. You can toggle the visibility of
+            this header by clicking on the top of the page. <br />
             <div style={{ color: "var(--pink)", textAlign: "center" }}>
               {" "}
               Feel free to check the code! :)
