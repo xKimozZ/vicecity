233dbe51ea8a395def23453eceeb710ab4b22bfd
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-02-01 02:30:24 +0200

    Add portrait mode unavailability message

--- src/components/Frontend/Frontend.jsx
+++ src/components/Frontend/Frontend.jsx
@@ -7,6 +7,13 @@ const Frontend = () => {
   return (
     <div id={FRONTEND_ROOT_ID}>
       <Landscape />
+      <div className="phone arborcrest arborcrestM">
+        <div className="phoneContent">
+        Portrait support currently unavailable.
+        <br />
+        <p style={{textAlign:"left"}}>Please rotate your device into landscape mode or increase the viewport width, or preferably access from a computer.</p>
+        </div>
+      </div>
     </div>
   );
 }
