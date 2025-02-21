3f1f6edc81db950a0c70c6cf2f90d55446ab6ffc
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-02-21 18:06:19 +0200

    Refactor image imports and reorganize control assets; remove unused background images

--- src/assets/imageImports.js
+++ src/assets/imageImports.js
@@ -1,10 +1,10 @@
 import vclogo1024 from './images/vclogo1024.png'
 import scsize from './images/scsize.png'
-import controller from './images/controller256.png'
-import arrows1 from './images/fe_arrows1.png'
-import arrows2 from './images/fe_arrows2.png'
-import arrows3 from './images/fe_arrows3.png'
-import arrows4 from './images/fe_arrows4.png'
+import controller from './images/controls/controller256.png'
+import arrows1 from './images/controls/fe_arrows1.png'
+import arrows2 from './images/controls/fe_arrows2.png'
+import arrows3 from './images/controls/fe_arrows3.png'
+import arrows4 from './images/controls/fe_arrows4.png'
 
 export const imageImports = {
   global: {
