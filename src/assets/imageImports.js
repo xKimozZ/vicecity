0725b716fbf5ac2f51e2302008d4168d4f45e5fb
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2026-04-20 17:17:46 +0200

    Add icon imports

--- src/assets/imageImports.js
+++ src/assets/imageImports.js
@@ -18,6 +18,36 @@ import mapBot01 from './images/map/mapBot01.png'
 import mapBot02 from './images/map/mapBot02.png'
 import mapBot03 from './images/map/mapBot03.png'
 
+import mapArrow from './images/map/icons/arrow.png'
+import mapGun from './images/map/icons/gun.png'
+import mapHardware from './images/map/icons/hardware.png'
+import mapSpray from './images/map/icons/spray.png'
+import mapProperty from './images/map/icons/property.png'
+import mapPhone from './images/map/icons/phone.png'
+import mapTshirt from './images/map/icons/tshirt.png'
+import mapClub from './images/map/icons/club.png'
+import mapBoatyard from './images/map/icons/boatyard.png'
+import mapIcecream from './images/map/icons/icecream.png'
+import mapPrintworks from './images/map/icons/printworks.png'
+import mapFilmstudio from './images/map/icons/filmstudio.png'
+import mapKcabs from './images/map/icons/kcabs.png'
+import mapSunYard from './images/map/icons/SunYard.png'
+import mapTommy from './images/map/icons/tommy.png'
+import mapBikers from './images/map/icons/bikers.png'
+import mapCubans from './images/map/icons/cubans.png'
+import mapHaitians from './images/map/icons/haitians.png'
+import mapLovefist from './images/map/icons/lovefist.png'
+import mapRadarSave from './images/map/icons/radar_save.png'
+import mapAvery from './images/map/icons/radar_avery.png'
+import mapBiker from './images/map/icons/radar_biker.png'
+import mapCortez from './images/map/icons/radar_cortez.png'
+import mapDiaz from './images/map/icons/radar_diaz.png'
+import mapKent from './images/map/icons/radar_kent.png'
+import mapLawyer from './images/map/icons/radar_lawyer.png'
+import mapPhil from './images/map/icons/radar_phil.png'
+import mapSave from './images/map/icons/radar_save.png'
+import mapStrip from './images/map/icons/radar_strip.png'
+
 export const imageImports = {
   global: {
     vclogo1024,
@@ -36,6 +66,37 @@ export const imageImports = {
     mapBot01,
     mapBot02,
     mapBot03,
+  },
+  mapIcons: {
+    arrow: mapArrow,
+    gun: mapGun,
+    hardware: mapHardware,
+    spray: mapSpray,
+    property: mapProperty,
+    phone: mapPhone,
+    tshirt: mapTshirt,
+    club: mapClub,
+    boatyard: mapBoatyard,
+    icecream: mapIcecream,
+    printworks: mapPrintworks,
+    filmstudio: mapFilmstudio,
+    kcabs: mapKcabs,
+    sunyard: mapSunYard,
+    tommy: mapTommy,
+    bikers: mapBikers,
+    cubans: mapCubans,
+    haitians: mapHaitians,
+    lovefist: mapLovefist,
+    save: mapRadarSave,
+    avery: mapAvery,
+    biker: mapBiker,
+    cortez: mapCortez,
+    diaz: mapDiaz,
+    kent: mapKent,
+    lawyer: mapLawyer,
+    phil: mapPhil,
+    radarSave: mapSave,
+    strip: mapStrip,
   },
   miscImages: {
     scsize,
